079196ccbb2c9d4400aca3bc981d40114efc0051
Alvizo07 <al291339@edu.uaa.mx>
2022-12-10 11:45:42 -0600

    Agregue el nav y footer a las paginas de formas de pago

--- pagoOxxo.php
+++ pagoOxxo.php
@@ -1,9 +1,15 @@
 <html>
     <head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 		<link href="css/style7.css" media="all" rel="stylesheet" type="text/css">
 		<title>Pago Oxxo</title>
 	</head>
 	<body>
+	<?php
+		include "navbar.php";
+		date_default_timezone_set('America/Mexico_City');
+		echo "Ultima actualizacion: " . date("F d Y H:i:s.", filemtime(__FILE__));
+    ?>
 		<div class="opps">
 			<div class="opps-header">
 				<div class="opps-reminder">Ficha digital. No es necesario imprimir.</div>
@@ -41,4 +47,7 @@
 	<?php
     include "html/footer.html";
     ?>
+	<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
 </html>	
