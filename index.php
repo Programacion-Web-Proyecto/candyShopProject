6872b5b7b683e7900bd68312c3c54c2823821224
VictorPrado002 <victor000hugo10@gmail.com>
2022-12-10 17:29:58 -0600

    correo de suscripcion listo

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 
 <body>
     <?php
-//    error_reporting(E_ERROR);
+    //    error_reporting(E_ERROR);
     include "navbar.php";
     session_start();
     ?>
@@ -32,28 +32,88 @@
     ?>
     <script>
         function entrada() {
-    swal( {
-      title: "Suscribete ahora!",
-      text: "Ingresa tu correo",
-      content: "input",
-      buttons:["CANCELAR", "SUSCRIBIRME"],
-       
-    }).then((value) => {
-      if (!value) throw null;
-        var direccion = value;
-        swal(`You typed: ${direccion}`);
-      
-        
-      
-    });
-  }
+            swal({
+                title: "Suscribete ahora!",
+                text: "Ingresa tu correo",
+                content: "input",
+                buttons: ["CANCELAR", "SUSCRIBIRME"],
 
-  entrada();
+            }).then((value) => {
+                if (!value) throw null;
+                var direccion = value;
+                window.location.href = window.location.href + "?w1=" + direccion;
+                // swal(`You typed: ${direccion}`);
+
+
+
+            });
+        }
+
+        entrada();
     </script>
 
 
     <?php
+    if (isset($_GET["w1"])) {
+        $email = $_GET["w1"];
+        correoSuscrip($email);
+
+    } 
+    ?>
+
+
+    <?php
     include "html/footer.html";
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+    function correoSuscrip ($correo){
+    
+        require 'recursos/PHPMailer/src/PHPMailer.php';
+        require 'recursos/PHPMailer/src/SMTP.php';
+        require 'recursos/PHPMailer/src/Exception.php';
+    
+        $mail = new PHPMailer(true);
+        try {
+    
+    
+            $mail->isSMTP();
+            $mail->Host = 'smtp.gmail.com';
+            $mail->SMTPAuth = true;
+    
+            $mail->Username = 'wichotl64@gmail.com';
+            $mail->Password   = 'ipubwtvrhzyyqhly';
+    
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+            $mail->Port = 587;
+    
+            $mail->setFrom('wichotl64@gmail.com', 'Candy Shop Mx');
+            $mail->addAddress($correo, 'Gracias por suscribirte!');
+    
+            $mail->addCC($correo);
+    
+            $mail->isHTML(true);
+    
+            $mail->Subject = 'Gracias por suscribirte!';
+    
+            $mail->Body = 'Gracias por su suscripcion a nuestra gran familia, CandyShopMx le agradece con un cupon de descuento por nuevo usuario! :';
+    
+            $mail->send();
+    
+            // echo 'Correo electronico enviado con exito';
+            header("Location: suscripcionExito.php");
+           
+        } catch (Exception $e) {
+            echo $mail->ErrorInfo;
+        }
+    }
+    
+
+
+
+
+
 
     ?>
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
